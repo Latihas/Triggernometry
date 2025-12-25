677a172a9a87e2abd7736fd46ef82d9f19a5663b
Latihas <lyjp2002@163.com>
2025-12-26 01:03:53 +0800

    update

---
+++ Source/Triggernometry/PScript/IScriptBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using static Triggernometry.PScript.ScriptUtils;
+
+namespace Triggernometry.PScript;
+
+public abstract class IScriptBase
+{
+    public bool Enabled;
+    public abstract string RegionIdRegex();
+    public abstract void Main(string log);
+    public virtual List<TargetIcon> TargetIconList => [];
+    public virtual List<TargetIcon> StartsCastingList => [];
+}
