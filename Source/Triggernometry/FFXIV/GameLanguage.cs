f8c536e2be405a7a79a540ab7050704922d33e28
Latihas <lyjp2002@163.com>
2026-05-27 16:07:12 +0800

    u

--- Source/Triggernometry/FFXIV/GameLanguage.cs
+++ Source/Triggernometry/FFXIV/GameLanguage.cs
@@ -1,40 +1,14 @@
-﻿using System;
-using Triggernometry.Utilities;
+﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
 
 namespace Triggernometry.FFXIV;
 
 public static class GameLanguage {
-	private static IntPtr _frameworkPtrPtr = IntPtr.Zero;
-
 	internal static void ScanOffsets() {
-		var moduleData = Memory.ReadModuleData(Memory.XivProc);
-		var offset = Memory.ScanPoint(moduleData, "49 8B C4 48 8B 0D ? ? ? ? 48 8D 15 ? ? ? ? 48 89 05 * * * *", false) // 7.0 CN
-		             ?? Memory.ScanPoint(moduleData, "49 8B DC 48 89 1D * * * *", false); // 7.0 global
-		if (!offset.HasValue) {
-			_frameworkPtrPtr = IntPtr.Zero;
-			return;
-		}
-		_frameworkPtrPtr = Memory.XivBaseAddress + offset.Value;
 	}
 
-	public static IntPtr FrameworkPtr {
-		get {
-			if (_frameworkPtrPtr == IntPtr.Zero) {
-				return IntPtr.Zero;
-			}
-			return Memory.Read<IntPtr>(Memory.XivProcHandle, _frameworkPtrPtr);
-		}
-	}
+	public static unsafe Framework* FrameworkPtr => Framework.Instance();
 
-	public static GameLanguageEnum Language {
-		get {
-			if (FrameworkPtr == IntPtr.Zero) {
-				return GameLanguageEnum.None;
-			}
-			var language = Memory.Read<byte>(Memory.XivProcHandle, FrameworkPtr + 0x580);
-			return (GameLanguageEnum)language;
-		}
-	}
+	public static unsafe GameLanguageEnum Language => (GameLanguageEnum)FrameworkPtr->ClientLanguage;
 }
 
 public enum GameLanguageEnum : byte {
